6fe7cf7e38872268d908172f32bcd74daaed33f4
Jarek Karwowski <chromedivizer@gmail.com>
2022-03-02 23:15:25 +0100

    nav - deleted 2dnav_STATE launches (too generic), contents composed into main STATE launches; localization & mapping configuration files properly loaded from nav_base; algorithm-specific launches moved to separate directory

--- tiago_social_navigation/scripts/navigation.sh
+++ tiago_social_navigation/scripts/navigation.sh
@@ -88,14 +88,37 @@ else
   LOCALIZATION_POSE_ESTIMATE=$12
 fi
 
+if [ $# -lt 13 ]; then
+  echo "[tiago_social_navigation/navigation.sh] You have to provide a path to configuration file for the localization component, use 'gazebo_nav_base.launch'"
+  exit 3
+else
+  LOCALIZATION_CONFIG_FILE=$13
+fi
+
+if [ $# -lt 14 ]; then
+  echo "[tiago_social_navigation/navigation.sh] You have to provide a path to configuration file for the mapping component, use 'gazebo_nav_base.launch'"
+  exit 4
+else
+  MAPPING_CONFIG_FILE=$14
+fi
+
 # Ensure target file exists
 if [ "$skip_map_operations" = "0" ]; then
     if [ ! -f "$MAP" ]; then
         echo "[tiago_social_navigation/navigation.sh] Error: Cannot find path: $MAP"
-        exit 3
+        exit 5
     fi
 fi
 
+if [ "$STATE" = "localization" ]; then
+  CONFIGURATION_FILE=$LOCALIZATION_CONFIG_FILE
+elif [ "$STATE" = "mapping" ]; then
+  CONFIGURATION_FILE=$MAPPING_CONFIG_FILE
+else
+  echo "[tiago_social_navigation/navigation.sh] Error: Cannot select configuration file because wrong STATE was given ($STATE)"
+  exit 6
+fi
+
 # Run localization/mapping
-echo "[tiago_social_navigation/navigation.sh] Invoking: 'roslaunch ${ROBOT}_social_navigation $STATE.launch localization:=$LOCALIZATION mapping:=$MAPPING map:=$MAP multiple:=$MULTI robot_namespace:=$ROBOT_NAMESPACE map_topic:=$MAP_TOPIC pose_estimate_file:=$LOCALIZATION_POSE_ESTIMATE'"
-roslaunch ${ROBOT}_social_navigation $STATE.launch localization:=$LOCALIZATION mapping:=$MAPPING map:=$MAP multiple:=$MULTI robot_namespace:=$ROBOT_NAMESPACE map_topic:=$MAP_TOPIC pose_estimate_file:=$LOCALIZATION_POSE_ESTIMATE
+echo "[tiago_social_navigation/navigation.sh] Invoking: 'roslaunch ${ROBOT}_social_navigation $STATE.launch localization:=$LOCALIZATION mapping:=$MAPPING map:=$MAP multiple:=$MULTI robot_namespace:=$ROBOT_NAMESPACE map_topic:=$MAP_TOPIC pose_estimate_file:=$LOCALIZATION_POSE_ESTIMATE config_file:=$CONFIGURATION_FILE'"
+roslaunch ${ROBOT}_social_navigation $STATE.launch localization:=$LOCALIZATION mapping:=$MAPPING map:=$MAP multiple:=$MULTI robot_namespace:=$ROBOT_NAMESPACE map_topic:=$MAP_TOPIC pose_estimate_file:=$LOCALIZATION_POSE_ESTIMATE config_file:=$CONFIGURATION_FILE
